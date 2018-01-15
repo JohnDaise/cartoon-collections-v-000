e9a3b74d1e03ae77ce5019e9f8962b71b76ea83d
John Daise <jdaise@gmail.com>
2018-01-15 04:22:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if food.include?("cheedar")
+  
+
+   food.include?("cheedar")
     puts "cheddar"
   else
   end
